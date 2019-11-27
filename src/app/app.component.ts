fe4d15d09fd1558b4038c0d46ba03a858efdd1f8
Henri Bresteau <henri.bresteau@gmail.com>
2019-11-27 14:51:22 +0100

    Method firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'book';
+
+  constructor(){
+    var firebaseConfig = {
+      apiKey: "AIzaSyAC2O8hMKr1jpU_n7gGnWNX18dJDX2fIL0",
+      authDomain: "book-angular-a49ed.firebaseapp.com",
+      databaseURL: "https://book-angular-a49ed.firebaseio.com",
+      projectId: "book-angular-a49ed",
+      storageBucket: "book-angular-a49ed.appspot.com",
+      messagingSenderId: "374822529160",
+      appId: "1:374822529160:web:ad3bbf3cdb38a4ff9f973d"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
